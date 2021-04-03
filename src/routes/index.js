810862fa9435cf366919e168b0933df70a7baf86
Marcelo Lima <jmdl.k8@gmail.com>
2021-04-02 21:49:38 -0300

    Feat: add is alive route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,16 @@ import authRoutes from './auth.routes';
 
 const routes = Router();
 
+routes.get('/', (request, response) => {
+  return response.json({
+    Author: 'Marcelo Lima',
+    Github: 'https://github.com/mrlsk8',
+    Project: 'Node API',
+    Version: '1.0.0',
+    Status: 'Online',
+  });
+});
+
 routes.use('/register', registerRoutes);
 
 routes.use('/auth', authRoutes);
