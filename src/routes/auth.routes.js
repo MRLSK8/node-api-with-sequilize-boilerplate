d6a3d0029be6d5b1718986f81f6bcaa4d2869616
Marcelo Lima <jmdl.k8@gmail.com>
2021-04-03 11:10:17 -0300

    feat: add validation to user login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import { validationMiddleware } from '../middlewares';
 import { AuthController } from '../app/controllers';
+import { loginValidator } from '../validators';
 
 const authRoutes = Router();
 
-authRoutes.post('/', AuthController.store);
+authRoutes.post('/', validationMiddleware(loginValidator), AuthController.store);
 
 export default authRoutes;
