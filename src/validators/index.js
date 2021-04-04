5f8fc2f212a2aecb1cbd207b3a4461c37f618196
Marcelo Lima <jmdl.k8@gmail.com>
2021-04-04 00:29:00 -0300

    refactor: import order fix

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,3 +1,3 @@
 export { default as registerValidator } from './registerValidator';
-export { default as loginValidator } from './loginValidator';
 export { default as updateValidator } from './updateValidator';
+export { default as loginValidator } from './loginValidator';
